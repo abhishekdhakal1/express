e44fcf1cf3e357af072c2f0c7eedfe75aa90db91
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-08-03 10:00:23 +0545

    route-params

--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 const express = require("express");
 const app = express();
 
+const mockdata = [
+  {
+    id: 1,
+    first_name: "Mariquilla",
+    last_name: "Hartzenberg",
+  },
+  {
+    id: 2,
+    first_name: "Jesse",
+    last_name: "Munkley",
+  },
+  {
+    id: 3,
+    first_name: "Fae",
+    last_name: "Gilchriest",
+  },
+  {
+    id: 4,
+    first_name: "Marketa",
+    last_name: "Morling",
+  },
+];
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,13 +34,18 @@ app.use((req, res, next) => {
 });
 
 //route
-app
-  .get("/", (req, res) => {
-    res.send("Server backend at home page.");
-  })
-  .get("/about", (req, res, next) => {
-    return next(new Error("This is not implemented"));
-  });
+app.get("/api/about", (req, res) => {
+  res.status(200).send(mockdata);
+});
+
+app.get("/api/about/:id", (req, res) => {
+  console.log(req.params);
+  const parsedId = parseInt(req.params.id);
+  //console.log(parsedId);
+  const user = mockdata.find((user) => user.id === parsedId);
+  if (!user) return res.status(404).send({ msg: "bad request" });
+  return res.send(user);
+});
 
 //error handler
 app.use((err, req, res, next) => {
