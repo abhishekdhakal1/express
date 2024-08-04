45de5b3a96b72fae2ed6c6c9a4deb5435c993e62
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-08-04 19:36:21 +0545

    added  mockdata.json

--- index.js
+++ index.js
@@ -2,47 +2,10 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mockdata = require("./mockdata.json");
+const {query} = require("express-validator");
 
-const mockdata = [
-  {
-    id: 1,
-    first_name: "Mariquilla",
-    last_name: "Hartzenberg",
-  },
-  {
-    id: 2,
-    first_name: "Jesse",
-    last_name: "Munkley",
-  },
-  {
-    id: 3,
-    first_name: "Fae",
-    last_name: "Gilchriest",
-  },
-  {
-    id: 4,
-    first_name: "Market",
-    last_name: "Morling",
-  },
-  {
-    id: 5,
-    first_name: "Kzerallaa",
-    last_name: "Leong",
-  },
-  {
-    id: 6,
-    first_name: "Hjerketa",
-    last_name: "Koling",
-  },
-];
-
-//middleware
-// const loggingMiddleware = (req, res, next) =>{
-//  console.log(`${req.method}-${req.url}`)
-//  next();
-// };
-
-const findUserIndex = (req, res, next) =>{
+const findUserIndex = (req, res, next) => {
   const {
     params: { id },
   } = req;
@@ -50,18 +13,15 @@ const findUserIndex = (req, res, next) =>{
   const findUserIndex = mockdata.findIndex((user) => user.id === parsedId);
   req.findUserIndex = findUserIndex;
   next();
-}
-
-//app.use(loggingMiddleware);
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  console.log("This is middleware.");
-  next(); // forward the request
-});
 
 //route
+app.get("/", (req, res) => {
+  res.send("Hello from express.");
+});
 app.get("/api/users", (req, res) => {
   const {
     query: { filter, value },
@@ -82,26 +42,14 @@ app.post("/api/users", (req, res) => {
 
 //route params
 app.get("/api/users/:id", (req, res) => {
-  //console.log(req.params);
   const parsedId = parseInt(req.params.id);
-  //console.log(parsedId);
-  //console.log(req.query);
   const user = mockdata.find((user) => user.id === parsedId);
   if (!user) return res.status(404).send({ msg: "bad request" });
   return res.send(user);
 });
 
-//error handler
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something broke! We don't know");
-// });
-
-app.patch("/api/users/:id",findUserIndex, (req, res) => {
-  const {
-    body,
-    findUserIndex,
-  } = req;
+app.patch("/api/users/:id", findUserIndex, (req, res) => {
+  const { body, findUserIndex } = req;
 
   if (findUserIndex === -1) {
     return res.status(400).send("User not found");
@@ -111,12 +59,8 @@ app.patch("/api/users/:id",findUserIndex, (req, res) => {
   return res.status(200).send(mockdata[findUserIndex]);
 });
 
-app.delete("/api/users/:id",findUserIndex, (req, res) => {
-  const {
-    findUserIndex
-  } = req;
-  //const parsedId = parseInt(id);
-  //const findUserIndex = mockdata.findIndex((user) => user.id === parsedId);
+app.delete("/api/users/:id", findUserIndex, (req, res) => {
+  const { findUserIndex } = req;
   if (findUserIndex === -1) {
     return res.status(400).send("User not found");
   }
@@ -124,7 +68,6 @@ app.delete("/api/users/:id",findUserIndex, (req, res) => {
   return res.status(200);
 });
 
-//port
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}`);
 });
