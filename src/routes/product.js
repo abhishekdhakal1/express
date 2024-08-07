cdbc8460980ff51f2e1f8f358a88eac0564ccd0a
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-08-07 19:24:35 +0545

    signed cookies

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/api/product", (req, res) => {
-  if (req.cookies.hello && req.cookies.hello === "world")
+  if (req.signedCookies.hello && req.signedCookies.hello === "world")
     return res.status(201).send([{ id: 1, product: "chicken" }]);
   return res.send({ msg: "Sorry but this is not available due to cookies" });
 });
