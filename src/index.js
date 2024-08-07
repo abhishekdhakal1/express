b2b4e6f4ab3f46fc5e19cc1ddbd69135cd323c6e
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-08-07 19:20:09 +0545

    used cookie parser for parsing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const cookieParser = require("cookie-parser");
 
-const { router : routes } = require("./routes/index.js");
+const { router: routes } = require("./routes/index.js");
+const { cookie } = require("express-validator");
 
 app.use(express.json());
+app.use(cookieParser('secret'));
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.get("/", (req, res) => {
+  res.cookie("hello", "world", { maxAge: 60000, signed: true });
   res.send("Hello from express.");
 });
 app.listen(PORT, () => {
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/api/product", (req, res) => {
-  res.status(201).send([{ id: 1, product: "unknown" }]);
+  if (req.cookies.hello && req.cookies.hello === "world")
+    return res.status(201).send([{ id: 1, product: "chicken" }]);
+  return res.send({ msg: "Sorry but this is not available due to cookies" });
 });
 
 module.exports = { router };
